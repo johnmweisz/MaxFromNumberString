8d0326b80f9ebcecc18736422b213de4c78266b2
John Weisz <johnmweisz@gmail.com>
2019-12-10 16:04:59 -0800

    Add project files.

---
+++ MaxFromNumberString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxFromNumberString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("type a list of numbers separated by a comma.");
+            var input = Console.ReadLine();
+            var numbers = input.Split(',');
+            var max = Convert.ToInt32(numbers[0]);
+            foreach (var stringNum in numbers)
+            {
+                var checkNum = Convert.ToInt32(stringNum);
+                if (checkNum > max) max = checkNum;
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
